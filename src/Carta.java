905133b79964cdad58b425355e63b7e008325c59
DFPG2305 <diego.perez.gonzalez@correounivalle.edu.co>
2026-03-23 17:49:59 -0500

    Carta.java

--- src/Carta.java
+++ src/Carta.java
@@ -1,14 +1,8 @@
-public class Carta {
-    String nombre;
-    byte nivel;
-    short ataque;
-    short vida;
+public abstract class Carta {
+    private String nombre;
 
-    public Carta(String nombre, byte nivel, short ataque, short vida) {
+    public Carta(String nombre) {
         this.nombre = nombre;
-        this.nivel = nivel;
-        this.ataque = ataque;
-        this.vida = vida;
     }
 
     public String getNombre() {
@@ -18,29 +12,4 @@ public class Carta {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
-    public byte getNivel() {
-        return nivel;
-    }
-
-    public void setNivel(byte nivel) {
-        this.nivel = nivel;
-    }
-
-    public short getAtaque() {
-        return ataque;
-    }
-
-    public void setAtaque(short ataque) {
-        this.ataque = ataque;
-    }
-
-    public short getVida() {
-        return vida;
-    }
-
-    public void setVida(short vida) {
-        this.vida = vida;
-    }
-
 }
